3c48856de9ddc4e354efba9a4775e91498f88ff8
carvalhorafael89 <rafael_cidao@yahoo.com.br>
2022-10-21 16:44:01 -0300

    evento do botao detalhes funcionando

--- cadastro_alunos/src/app/alunos/alunos.component.ts
+++ cadastro_alunos/src/app/alunos/alunos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectMultipleControlValueAccessor } from '@angular/forms';
+import { aluno } from 'src/aluno';
 import { mockalunos } from 'src/mock-alunos';
 
 @Component({
@@ -6,12 +8,19 @@ import { mockalunos } from 'src/mock-alunos';
   templateUrl: './alunos.component.html',
   styleUrls: ['./alunos.component.css']
 })
-export class lista_alunos implements OnInit {
+
+export class lista_alunos_class implements OnInit {
 
   lista_alunos = mockalunos;
+  alunoselect?: aluno;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  seleciona(lista_alunos: aluno): void
+  {
+    this.alunoselect = lista_alunos;
   }
 }
--- cadastro_alunos/src/app/app.module.ts
+++ cadastro_alunos/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { lista_alunos } from './alunos/alunos.component';
+import { lista_alunos_class } from './alunos/alunos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    lista_alunos,
+    lista_alunos_class,
   ],
   imports: [
     BrowserModule,
