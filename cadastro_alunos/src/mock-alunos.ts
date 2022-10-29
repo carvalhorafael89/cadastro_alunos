0c95946f9032911e3dbf2c6b966b1a591ed7fa49
carvalhorafael89 <rafael_cidao@yahoo.com.br>
2022-10-29 02:14:53 -0300

    trocas de nomes do mock-alunos para uns mais ok

--- cadastro_alunos/src/mock-alunos.ts
+++ cadastro_alunos/src/mock-alunos.ts
@@ -1,14 +1,14 @@
 import { aluno_int } from '../src/aluno';
 
 export const mockalunos: aluno_int[] = [
-  { id: 1, ra: 123, nome: 'Maroteiro', curso: 'ADS', idade: 23 },
-  { id: 2, ra: 456, nome: 'Gatinha tímida', curso: 'Eventos', idade: 18 },
-  { id: 3, ra: 789, nome: 'Pimpão', curso: 'Eventos', idade: 20 },
-  { id: 4, ra: 111, nome: 'Cara que cola', curso: 'ADS', idade: 19 },
-  { id: 5, ra: 222, nome: 'Otaku Fedido', curso: 'ADS', idade: 18 },
-  { id: 6, ra: 333, nome: '"Adultão"', curso: 'ADS', idade: 25 },
-  { id: 7, ra: 444, nome: 'Sempre Reclama', curso: 'ADS', idade: 23 },
-  { id: 8, ra: 555, nome: 'Ninja de Gibi', curso: 'ADS', idade: 22 },
-  { id: 9, ra: 666, nome: 'Cara babacão mas manja', curso: 'ADS', idade: 23 },
-  { id: 10, ra: 777, nome: 'Cara "olhem pra mim"', curso: 'ADS', idade: 18 },
+  { id: 1, ra: 123, nome: 'Fernandinho', curso: 'ADS', idade: 23 },
+  { id: 2, ra: 456, nome: 'Gustavo', curso: 'Eventos', idade: 18 },
+  { id: 3, ra: 789, nome: 'André Freitas', curso: 'Eventos', idade: 20 },
+  { id: 4, ra: 111, nome: 'Beatriz', curso: 'ADS', idade: 19 },
+  { id: 5, ra: 222, nome: 'Marina', curso: 'ADS', idade: 18 },
+  { id: 6, ra: 333, nome: 'Leila', curso: 'ADS', idade: 25 },
+  { id: 7, ra: 444, nome: 'Carlos', curso: 'ADS', idade: 23 },
+  { id: 8, ra: 555, nome: '"Aquele cara que sempre tá jogando"', curso: 'ADS', idade: 22 },
+  { id: 9, ra: 666, nome: 'Henrique', curso: 'ADS', idade: 23 },
+  { id: 10, ra: 777, nome: 'Carla', curso: 'ADS', idade: 18 },
 ];
